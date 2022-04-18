91e51f9bc64172a67aa21fceafe700180691f14c
samisbakedham <samsafahi@gmail.com>
2022-04-18 00:51:45 -0700

    fix

--- constants/hardhat.js
+++ constants/hardhat.js
@@ -1,18 +1,18 @@
 exports.WRAPPED_NATIVE_TOKEN;
-exports.PNG_SYMBOL = "PNG";
-exports.PNG_NAME = "Pangolin";
-exports.TOTAL_SUPPLY = 230000000; // 230M
-exports.AIRDROP_AMOUNT = 11500000; // 11.5M or 5% of max supply
+exports.PNG_SYMBOL = "HOL";
+exports.PNG_NAME = "Hollis";
+exports.TOTAL_SUPPLY = 6900000000; // 230M
+exports.AIRDROP_AMOUNT = 42000000; // 11.5M or 5% of max supply
 exports.TIMELOCK_DELAY = 3 * 24 * 60 * 60; // 3 days
 exports.MULTISIG = {
   owners: [
-    "0x72C397908Cb93d1B569BBB0Ff8d3D26B7b21d730", // Trollip
-    "0xDA315a838E918026E51A864c43766f5AE86be8c6"  // Shung
+    "0xCEcAcEbcc595276f7a707fb6772016C0Ea1ac725", // Trollip
+    "0x05DDF0637F80C5f156Dc415e2305aD4d9A2127b5"  // Shung
   ],
   threshold: 2
 };
 exports.USE_GNOSIS_SAFE = false;
-exports.PROPOSAL_THRESHOLD = 1000000; // 1M
+exports.PROPOSAL_THRESHOLD = 20000000; // 1M
 exports.PNG_STAKING_ALLOCATION = 500, // 5x weight in minichef
 exports.WETH_PNG_FARM_ALLOCATION = 3000, // 30x weight
 exports.INITIAL_FARMS = [
@@ -24,21 +24,21 @@ exports.VESTER_ALLOCATIONS = [
   },
   {
     recipient: "multisig", // team
-    allocation: 1579, // 10% team + 5% vc investor
+    allocation: 4579, // 10% team + 5% vc investor
   },
   {
-    recipient: "foundation", // PNG Foundation multisig
+    recipient: "foundation", // HOL Foundation multisig
     allocation: 263, // 2.5% advisory
   },
   {
     recipient: "chef", // MiniChef
-    allocation: 6053, // 57.5% LPs & PNG Staking
+    allocation: 3053, // 57.5% LPs & HOLS Staking
     isMiniChef: true
   }
 ];
 exports.REVENUE_DISTRIBUTION = [
   {
-    recipient: "foundation", // Pangolin Foundation
+    recipient: "foundation", // Hollis Foundation
     allocation: 2000,        // 20%
   },
   {
